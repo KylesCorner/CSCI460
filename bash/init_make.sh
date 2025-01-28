6da6c77bf44cb33b7ef6cc4a9fdca847c302b2d3
Kyle <kylekrstulich@gmail.com>
2025-01-28 22:34:04 +0000

    Documentation

--- bash/init_make.sh
+++ bash/init_make.sh
@@ -8,6 +8,12 @@
 # by aggressively assigning all the available system resources 
 # for compilation. Needs sudo permission to function.
 #
+# For those in CSCI460 with me, run this script instead of these commands:
+#	
+#		cd /home/kernel/linux-hwe-4.15.0
+#		sudo make -j2 CC="ccache gcc"
+#		sudo make -j2 modules_install
+#		sudo make -j2 install
 
 # Define the path to your log folder. These will have to be changed
 # depending on your system.
@@ -15,6 +21,7 @@ LOG_DIR="/home/kyle/scripts/bash/logs/"
 KERNEL_DIR="/home/kernel/linux-6.8/"
 
 # Create the log directory if it doesn't exist
+# There's a bug in this script where you need sudo permission to edit the /logs/ directory
 mkdir -p "$LOG_DIR"
 
 # Define the build steps
@@ -34,9 +41,11 @@ for NAME in "${COMMANDS[@]}"; do
 		# Removes unnecessary/unutilized object references from MakeFile
 		echo "Cleaning up old objects..."
 		if make -j$(nproc) clean >"$OUTPUT_LOG" 2> "$ERROR_LOG"; then
-			echo "$Name Completed. Continuing..."
+			echo "Clean completed. Continuing..."
 		else
 			echo "Error detected in $ERROR_LOG. Stopping further builds."
+			echo "Here's a sample of the error log."
+			head -n 10 $(ERROR_LOG)
 			exit 1
 		fi
             ;;
@@ -45,9 +54,11 @@ for NAME in "${COMMANDS[@]}"; do
 		# Compiles C files using the ccache and gcc compilers
 		echo "Compiling, this will take a while..."
 		if make -j$(nproc) CC="ccache gcc" >"$OUTPUT_LOG" 2>"$ERROR_LOG"; then
-			echo "$Name Completed. Continuing..."
+			echo "Compilation completed. Continuing..."
 		else
 			echo "Error detected in $ERROR_LOG. Stopping further builds."
+			echo "Here's a sample of the error log."
+			head -n 10 $(ERROR_LOG)
 			exit 1
 		fi
             ;;
@@ -56,20 +67,24 @@ for NAME in "${COMMANDS[@]}"; do
 		# Assuming all binaries compile without error, installs them to the kernels modules directory
 		echo "Installing modules..."
 		if make -j$(nproc) modules_install >"$OUTPUT_LOG" 2>"$ERROR_LOG"; then
-			echo "$Name Completed. Continuing..."
+			echo "Module install completed. Continuing..."
 		else
 			echo "Error detected in $ERROR_LOG. Stopping further builds."
+			echo "Here's a sample of the error log."
+			head -n 10 $(ERROR_LOG)
 			exit 1
 		fi
             ;;
 
         "final_install")
 		# Copies compiled binaries into their respective directories
-		echo "Finilizing install.."
+		echo "Finalizing install.."
 		if make -j$(nproc) install >"$OUTPUT_LOG" 2>"$ERROR_LOG"; then
-			echo "$Name Completed. Continuing..."
+			echo "Final install completed. Continuing..."
 		else
 			echo "Error detected in $ERROR_LOG. Stopping further builds."
+			echo "Here's a sample of the error log."
+			head -n 10 $(ERROR_LOG)
 			exit 1
 		fi
             ;;
