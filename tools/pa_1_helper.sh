daf57ba06403726e366a8aa10663b28ecc47fcba
Kyle <kylekrstulich@gmail.com>
2025-01-30 22:55:38 +0000

    pa1 helper file added

--- tools/pa_1_helper.sh
+++ tools/pa_1_helper.sh
@@ -1,9 +1,17 @@
 # pa_1_helper.sh
 #
-# This prints out the syscall made in programming assignment 1 part a
+# Zips up your assignment when finished.
+#
+# These variables need to be changed to fit your system.
 KERNEL_DIR="/home/kernel/linux-6.8/"
+TEST_PGM_DIR="/code/test_helloworld.c"
+
+# zip command zips a file with this syntax:
+#   zipi {arguments} FILE.zip FILES_TO_ZIP
+#
+# the -j option throws away the paths for the files in the zip folder
+sudo zip -j assignment.zip "$KERNEL_DIR/arch/x86/kernel/helloworld.c" "$TEST_PGM_DIR" "/var/log/syslog"
+
+unzip -l assignment.zip
 
 
-sudo tail /var/log/syslog | grep "Hello world!"
-cat "$KERNEL_DIR"arch/x86/entry/syscalls/syscall_64.tbl | grep helloworld
-cat "$KERNEL_DIR"arch/x86/kernel/helloworld.c
